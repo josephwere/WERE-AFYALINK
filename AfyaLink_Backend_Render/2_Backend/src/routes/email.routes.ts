4346354de193e1d8b4854617983c0c1603bc2efc
josephwere <josephogwe8@gmail.com>
2025-10-27 12:30:49 +0300

    Delete AfyaLink_Backend_Render directory

--- AfyaLink_Backend_Render/2_Backend/src/routes/email.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import * as EmailCtrl from "../controllers/email.controller";
-import { authenticate } from "../middleware/auth.middleware";
-
-const router = Router();
-
-router.post("/test", authenticate, EmailCtrl.testEmail);
-
-export default router;
--- AfyaLink_Backend_Render/src/utils/hashPassword.ts
+++
@@ -1,2 +0,0 @@
-import bcrypt from "bcryptjs";
-export const hash = (p: string) => bcrypt.hash(p, 10);
